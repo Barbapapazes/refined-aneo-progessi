98d6403835d0317b39aecc4428c2686f33df798f
barbapapazes <e.soubiran25@gmail.com>
2023-08-25 10:10:05 +0200

    chore: release v0.1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Refined Aneo Progessi
 // @namespace    http://tampermonkey.net/
-// @version      0.0.4
+// @version      0.1.0
 // @description  UserScript that enhances the Progessi of Aneo
 // @author       Estéban Soubiran (https://github.com/barbapapazes)
 // @license      MIT
